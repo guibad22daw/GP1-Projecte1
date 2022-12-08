63d40be6eb1acae66b401c91f46e8b21fa3969dc
guibad22daw <guibad22daw@proton.me>
2022-12-08 19:38:41 +0100

    Millora al servidor

--- iniciaServidor.js
+++ iniciaServidor.js
@@ -1,6 +1,7 @@
 let http = require("http");
 let fs = require('fs');
 var cookie = require('cookie');
+var idUsuari;
 
 let MongoClient = require('mongodb').MongoClient;
 let assert = require('assert'); //utilitzem assercions
@@ -12,109 +13,135 @@ let crud = {
     }
 };
 
-function onRequest(request, response) {
+function onRequest(req, res) {
     let sortida;
-    const baseURL = request.protocol + '://' + request.headers.host + '/';
-    const reqUrl = new URL(request.url, baseURL);
+    const baseURL = req.protocol + '://' + req.headers.host + '/';
+    const reqUrl = new URL(req.url, baseURL);
     console.log("Petició per a  " + reqUrl.pathname + " rebuda.");
     const ruta = reqUrl.pathname;
     let cadenaConnexio = 'mongodb://127.0.0.1:27017/GP1';
 
     if (ruta == '/') {
-        response.writeHead(301, {
+        res.writeHead(301, {
             Location: `/login`
         }).end();
     }
 
     else if (ruta == '/login') {
         fs.readFile('./login/login.html', function (err, sortida) {
-            response.writeHead(200, {
+            res.writeHead(200, {
                 "Content-Type": "text/html; charset=utf-8"
             });
-            response.write(sortida);
-            response.end();
+            res.write(sortida);
+            res.end();
         });
     }
     else if (ruta == '/logo.png') {
         fs.readFile('./login/logo.png', function (err, sortida) {
-            response.writeHead(200, {
-                "Content-Type": "text/css; charset=utf-8"
+            res.writeHead(200, {
+                "Content-Type": "image/png; charset=utf-8"
             });
-            response.write(sortida);
-            response.end();
+            res.write(sortida);
+            res.end();
         });
     }
     else if (ruta == '/login.js') {
         fs.readFile('./login/login.js', function (err, sortida) {
-            response.writeHead(200, {
-                "Content-Type": "text/css; charset=utf-8"
+            res.writeHead(200, {
+                "Content-Type": "text/javascript; charset=utf-8"
             });
-            response.write(sortida);
-            response.end();
+            res.write(sortida);
+            res.end();
         });
     }
     else if (ruta == '/imagen-fondo.png') {
         fs.readFile('./login/imagen-fondo.png', function (err, sortida) {
-            response.writeHead(200, {
+            res.writeHead(200, {
                 "Content-Type": "text/css; charset=utf-8"
             });
-            response.write(sortida);
-            response.end();
+            res.write(sortida);
+            res.end();
         });
     }
 
-    else if (ruta == '/index.css') {
-        fs.readFile('./calendari/index.css', function (err, sortida) {
-            response.writeHead(200, {
+    else if (ruta == '/calendari.css') {
+        fs.readFile('./calendari/calendari.css', function (err, sortida) {
+            res.writeHead(200, {
                 "Content-Type": "text/css; charset=utf-8"
             });
-            response.write(sortida);
-            response.end();
+            res.write(sortida);
+            res.end();
         });
     }
 
-    else if (ruta == '/index.js') {
-        fs.readFile('./calendari/index.js', function (err, sortida) {
-            response.writeHead(200, {
+    else if (ruta == '/calendari.js') {
+        fs.readFile('./calendari/calendari.js', function (err, sortida) {
+            res.writeHead(200, {
                 "Content-Type": "text/javascript; charset=utf-8"
             });
-            response.write(sortida);
-            response.end();
+            res.write(sortida);
+            res.end();
         });
     }
+
 
     else if (ruta == '/desa') {
         MongoClient.connect(cadenaConnexio, function (err, client) {
             assert.equal(null, err);
-            console.log("Connexió correcta");
+            console.log("Connexió correcta.");
             var db = client.db('GP1');
-            db.collection('proves').insertOne({
-                "nom": reqUrl.searchParams.get('nom'),
-                "password": reqUrl.searchParams.get('password')
-            }).then(result => {
-                console.log(result.insertedId);
-                var idUsuari = result.insertedId
-             });
-            assert.equal(err, null);
-            console.log("Afegit document a col·lecció proves");
-            // db.collection('proves').findOne({ nom: `${reqUrl.searchParams.get('nom')}` }, function (err, result) {
-            //     if (err) throw err;
-            //     var id = result._id;
-            // });
+            db.collection('proves').findOne({ nom: reqUrl.searchParams.get('nom') }).then(result => {
+                if (result) {
+                    console.log('Iniciant sessió...');
+                    idUsuari = result._id;
+                    console.log(idUsuari);
+                } else {
+                    db.collection('proves').insertOne({
+                        "nom": reqUrl.searchParams.get('nom'),
+                        "password": reqUrl.searchParams.get('password')
+                    }).then(result => {
+                        idUsuari = result.insertedId;
+                        console.log('Usuari creat amb ID ' + idUsuari);
+                    })
+                }
+            }).then(() => {                 
+                res.setHeader('Set-Cookie', cookie.serialize('id', idUsuari, {
+                    httpOnly: false,
+                    maxAge: 60 * 15 // 15 minuts
+                }));
+                res.statusCode = 301;
+                res.setHeader('Location', '/calendari');
+                res.end();
+            });
+        })
+        //     db.collection('proves').insertOne({
+        //         "nom": reqUrl.searchParams.get('nom'),
+        //         "password": reqUrl.searchParams.get('password')
+        //     }).then(result => {
+        //         idUsuari = result.insertedId;
+        //         console.log(idUsuari);
+        //     });
+        //     assert.equal(err, null);
+        //     console.log("Afegit document a col·lecció proves");
 
-        });
-        response.writeHead(301, {
-            Location: `/calendari`
-        }).end();
+        // });
     }
 
     else if (ruta == '/calendari') {
-        fs.readFile('./calendari/index.html', function (err, sortida) {
-            response.writeHead(200, {
+        let cookies = cookie.parse(req.headers.cookie || '');
+        console.log(cookies);
+        let name = cookies.id;
+        if (name) {
+            console.log('Benvingut ' + name);
+        } else {
+            console.log('Error llegint cookie.');
+        }
+        fs.readFile('./calendari/calendari.html', function (err, sortida) {
+            res.writeHead(200, {
                 "Content-Type": "text/html; charset=utf-8"
             });
-            response.write(sortida);
-            response.end();
+            res.write(sortida);
+            res.end();
         });
     }
 
@@ -124,7 +151,7 @@ function onRequest(request, response) {
             console.log("Connexió correcta");
             var db = client.db('GP1');
 
-            response.writeHead(200, {
+            res.writeHead(200, {
                 "Content-Type": "text/html; charset=utf-8"
             });
             console.log("consulta document a col·lecció usuaris");
@@ -135,10 +162,10 @@ function onRequest(request, response) {
                 assert.equal(err, null);
                 if (results != null) {
                     results.forEach((doc) => {
-                        response.write(`usuari: ${doc.nom} | password: ${doc.password} <br>`);
+                        res.write(`usuari: ${doc.nom} | password: ${doc.password} <br>`);
                     });
                 } else {
-                    response.end();
+                    res.end();
                 }
             }));
         });
@@ -147,47 +174,47 @@ function onRequest(request, response) {
     // INFO
     else if (ruta == '/informacio') {
         fs.readFile('./info/info.html', function (err, sortida) {
-            response.writeHead(200, {
+            res.writeHead(200, {
                 "Content-Type": "text/html; charset=utf-8"
             });
-            response.write(sortida);
-            response.end();
+            res.write(sortida);
+            res.end();
         });
     }
     else if (ruta == '/banc_temps.mp4') {
         fs.readFile('./info/banc_temps.mp4', function (err, sortida) {
-            response.writeHead(200, {
+            res.writeHead(200, {
                 "Content-Type": "video/mp4; charset=utf-8"
             });
-            response.write(sortida);
-            response.end();
+            res.write(sortida);
+            res.end();
         });
     }
     else if (ruta == '/info.css') {
         fs.readFile('./info/info.css', function (err, sortida) {
-            response.writeHead(200, {
+            res.writeHead(200, {
                 "Content-Type": "text/css; charset=utf-8"
             });
-            response.write(sortida);
-            response.end();
+            res.write(sortida);
+            res.end();
         });
     }
     else if (ruta == '/info.js') {
         fs.readFile('./info/info.js', function (err, sortida) {
-            response.writeHead(200, {
+            res.writeHead(200, {
                 "Content-Type": "text/js; charset=utf-8"
             });
-            response.write(sortida);
-            response.end();
+            res.write(sortida);
+            res.end();
         });
     }
     else {
-        response.writeHead(404, {
+        res.writeHead(404, {
             "Content-Type": "text/html; charset=utf-8"
         });
         sortida = "404 NOT FOUND";
-        response.write(sortida);
-        response.end();
+        res.write(sortida);
+        res.end();
     }
 }
 
